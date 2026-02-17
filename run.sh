85dfa0ff2cf92dcb838899832ad112c46f4e8193
Vlad <faceitakk51515@gmail.com>
2026-02-18 00:37:52 +0500

    add index.html check in run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 echo "Start archive..."
 docker-compose up
-echo "Ready. Check index.html"
+
+if [ -f "index.html" ]; then
+	echo "Ready. Check index.html"
+	ls -lh index.html
+else 
+	echo "Error cannot find index.html"
+fi
