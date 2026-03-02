078aae8c28ec7ae3a2493893d374b51cffb1e632
Vlad <faceitakk51515@gmail.com>
2026-03-03 04:27:41 +0500

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9-slim
 WORKDIR /app
-RUN pip install requests
+RUN pip install requests flask
 COPY app.py .
 CMD ["python", "app.py"]
 
--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    url = "https://google.com"
+    response = requests.get(url)
+    with open("index.html", "w") as f:
+        f.write(response.text)
+    return f"Status: {response.status_code}, OK"
+
+if __name__ == '__main__':
+    # ВАЖНО: host='0.0.0.0' обязателен для работы внутри Docker!
+    app.run(host='0.0.0.0', port=5000)
+
 import requests
 
 url = "https://google.com"
