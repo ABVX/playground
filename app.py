c55628dea5f16f48e7b09558340b0d3e7305de3a
Vlad <faceitakk51515@gmail.com>
2026-03-10 20:39:39 +0500

    fix

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 
 print("archiver is running")
 
-@app.route('/')
-def hello():
-    url = "https://github.com/"
+def download_page():
+    url = "https://google.com"
     response = requests.get(url)
+
     with open("index.html", "w") as f:
         f.write(response.text)
-    return f"Status: {response.status_code}, OK"
+
+@app.route('/')
+def hello():
+    download_page()
+    return "Status OK"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
