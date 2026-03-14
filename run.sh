dce92b46e378643212979ddcaba48ba019f07001
Vlad <faceitakk51515@gmail.com>
2026-03-15 01:20:42 +0500

    .

--- run.sh
+++ run.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-if  [ -f "index.html" ]; then
-	rm -f index.html
-	echo "Old index.html file will be deleted"
-fi
-
-echo "Start archive..."
-docker-compose up -d
-
-echo "waiting for archiver to finish..."
-until [ -f index.html ]
-do
-	sleep 1
-done
-echo "File found! Starting web server"
-
-if [ -f "index.html" ]; then
-	echo "Ready. Check index.html"
-	ls -lh index.html
-else 
-	echo "Error cannot find index.html"
-fi
+docker compose up -d
