509f64ceaf7c00b1175b3f5f3028723d330fafcf
Vlad <faceitakk51515@gmail.com>
2026-03-18 00:55:24 +0500

    add exception block to trap errors

--- tester.py
+++ tester.py
@@ -1,15 +1,20 @@
 import requests
+import sys
 
 def archiver_test():
     url = "http://localhost:8080"
 
     try:
         response = requests.get(url)
-        assert response.status_code == 200
-        print("First test success")
-
+        if response.status_code == 200:
+            print("ALL OK")
+            sys.exit(0)
+        else:
+            print(f"ERROR, CODE: {response.status_code}")
+            sys.exit(1)
     except Exception as e:
-        print(f"Test failed!: {e}")
+        print(f"NOT WORK: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     archiver_test()
