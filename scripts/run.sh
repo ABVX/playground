2272af3980b56dec98c6a851eec225365ea3da3a
Vlad <faceitakk51515@gmail.com>
2026-04-02 04:51:10 +0500

    fix path to docker-compose.yml

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-docker-compose up -d
+
+ARCHIVE_DIR="./data/archives"
+LOG_FILE="archiver.log"
+
+echo "preparation environment..."
+
+if [ ! -d "$ARCHIVE_DIR" ]; then
+    echo "folder $ARCHIVE_DIR not found. Creating..."
+    mkdir -p "$ARCHIVE_DIR"
+else
+    echo "folder is exist"
+fi
+
+echo "starting Docker..."
+docker-compose -f ../archiver/docker/docker-compose.yml up -d --build
+
+echo "service is ready on port 8080"
+echo "you can see logs in $LOG_FILE"
+date >> $LOG_FILE
