145095fcb537748a2f4960a2ff1df5f743f59cd7
Vlad <faceitakk51515@gmail.com>
2026-05-27 22:32:03 +0500

    wrap site checker into infinite loop with time delay

--- site-monitor/monitor.py
+++ site-monitor/monitor.py
@@ -1,18 +1,26 @@
 import requests
 import os
+import time
 
 sites_string = os.getenv("MONITOR_SITES", "https://google.com") # Приравниваю к response получение статуса сайта
 
 sites = sites_string.split(",")
 
-for site in sites:
-    try:
-        response = requests.get(site)
+while True:
+    print("Запуск круга проверки сайтов")
 
-        if response.status_code == 200: # .status_code возвращает цифровой код от сервера в виде числа
-            print (f"[OK] {site} available")
-        else:
-            print (f"[ERROR] {site} вернул код {response.status_code}")
 
-    except requests.exceptions.RequestException as Error:
-        print(f"[CRITICAL] Не удалось подключиться к {site}. Ошибка {Error}")
+    for site in sites:
+        try:
+            response = requests.get(site)
+
+            if response.status_code == 200: # .status_code возвращает цифровой код от сервера в виде числа
+                print (f"[OK] {site} available")
+            else:
+                print (f"[ERROR] {site} вернул код {response.status_code}")
+
+        except requests.exceptions.RequestException as Error:
+            print(f"[CRITICAL] Не удалось подключиться к {site}. Ошибка {Error}")
+
+    print("Ожидание 10 секунд до следующей проверки...\n")
+    time.sleep(10)
