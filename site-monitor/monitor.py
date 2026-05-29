8bb5bfb9898ab29eecc317fc8265ecc3ed661d9b
Vlad <faceitakk51515@gmail.com>
2026-05-30 00:51:40 +0500

    add prometheus manifest

--- site-monitor/monitor.py
+++ site-monitor/monitor.py
@@ -1,10 +1,15 @@
+from prometheus_client import start_http_server, Counter
 import requests
 import os
 import time
 
+REQUEST_COUNTER = Counter("site_checks_total", "Общее количество проверок сайтов")
+
 sites_string = os.getenv("MONITOR_SITES", "https://google.com") # Приравниваю к response получение статуса сайта
 
 sites = sites_string.split(",")
+
+start_http_server(8000)
 
 while True:
     print("Запуск круга проверки сайтов")
@@ -13,6 +18,7 @@ while True:
     for site in sites:
         try:
             response = requests.get(site)
+            REQUEST_COUNTER.inc()
 
             if response.status_code == 200: # .status_code возвращает цифровой код от сервера в виде числа
                 print (f"[OK] {site} available")
